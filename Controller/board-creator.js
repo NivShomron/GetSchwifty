c755f5fd4fb9831904b7a75c55d8b55b5f278edc
Niv <niv@shomron.co>
2022-07-03 17:51:29 +0300

    moved cells values creator code from class

--- Controller/board-creator.js
+++ Controller/board-creator.js
@@ -1,34 +1,13 @@
 class BoardCreator {
-    constructor(board, arrayShuffler, boardValidator) {
+    constructor(board, boardValidator, cellsValuesCreator) {
         this.board = board;
-        this.arrayShuffler = arrayShuffler;
         this.boardValidator = boardValidator;
-
-        var gameNumbers = [];
-
-        for(var i = 0; i < board.size; i++) {
-            matrix[i] = new Array(board.size);
-        }
-
-        for(var i = 0; i < board.size * board.size; i++) {
-            gameNumbers.push(i + 1);
-        }
-
-        this.removeRandom(gameNumbers);
-        this.shuffledGameNumbers = arrayShuffler.shuffle(gameNumbers);
+        this.cellsValues = cellsValuesCreator.create(this.board);
     }
-
-    removeRandom = (array) => {
-        while(array.length){
-           const random = Math.floor(Math.random() * array.length);
-           const el = array.splice(random, 1)[0];
-           console.log(el);
-        }
-    };
 
     create() {
         do {
-            shuffledGameNumbers.forEach(number => {
+            this.cellsValues.forEach(number => {
                 this.board.addCell(number)
             });
         } while (this.boardValidator.validate(this.board.getSwitchesCount()));
