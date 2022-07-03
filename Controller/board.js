8eb9d53fc02598a69d762ada3f5decb1dacf4933
Niv <niv@shomron.co>
2022-07-03 17:23:51 +0300

    created function to count the amount of switches the board needs

--- Controller/board.js
+++ Controller/board.js
@@ -4,12 +4,26 @@ class Board {
         this.board = [];
         this.size = size;
 
-        for(var i = 0; i < this.size; i++) {
+        for(let i = 0; i < this.size; i++) {
             this.board[i] = new Array(this.size);
         }
     }
 
     addCell(x, y, value) {
         board[x][y] = value;
+    }
+
+    getSwitchesCount(shuffledGameNumbers) {
+        var switchesCount = 0;
+        shuffledGameNumbers.forEach(number => {
+            for (let i = 0; i < this.size; i++) {
+                for (let j = i; i < this.size; j++) {
+                    if (board[i][j] < number) {
+                        switchesCount++;
+                    }
+                }
+            }
+        });
+        return switchesCount;
     }
 }
