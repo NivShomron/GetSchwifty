5640967e19c4e23815b346b53da1e574eace3212
Niv <niv@shomron.co>
2022-07-03 17:41:41 +0300

    created class to execute movement of cell

---
+++ Controller/movement-executer.js
@@ -0,0 +1,14 @@
+class MovementExecuter {
+    constructor(board) {
+        this.board = board;
+    }
+
+    execute(cellDecision) {
+        var cellValue = this.board[cellDecision[0]][cellDecision[1]];
+        var emptyCell = this.board.getEmptyCell();
+
+        this.board[emptyCell[0]][emptyCell[1]] = cellValue;
+        this.board[cellDecision[0]][cellDecision[1]] = undefined;
+        // display changes
+    }
+}
