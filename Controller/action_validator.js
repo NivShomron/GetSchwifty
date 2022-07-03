a703671a7f3a94533fc8b73b4cc7f3dea6fdaace
Niv <niv@shomron.co>
2022-07-03 17:42:23 +0300

    created class to validate cell movement

---
+++ Controller/action_validator.js
@@ -0,0 +1,10 @@
+class ActionValidator {
+    constructor(board) {
+        this.board = board;
+    }
+
+    validate(cellDecision) {
+        var emptyCell = this.board.getEmptyCell()
+        // check if cellDecision is adjacent to empty cell
+    }
+}
