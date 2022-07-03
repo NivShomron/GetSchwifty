b0b9c8780c8552c67102653d9ce14dc755f75233
Niv <niv@shomron.co>
2022-07-03 17:49:33 +0300

    created class to create cells values array ready to use

---
+++ Controller/cells_values_creator.js
@@ -0,0 +1,29 @@
+class CellsValuesCreator {
+    constructor(board, arrayShuffler) {
+        this.board = board;
+        this.arrayShuffler = arrayShuffler;
+    }
+
+    create() {
+        var gameNumbers = [];
+
+        for(var i = 0; i < this.board.size; i++) {
+            matrix[i] = new Array(this.board.size);
+        }
+
+        for(var i = 0; i < this.board.size * this.board.size; i++) {
+            gameNumbers.push(i + 1);
+        }
+
+        this.removeRandomValue(gameNumbers);
+        return this.arrayShuffler.shuffle(gameNumbers);
+    }
+
+    removeRandomValue = (array) => {
+        while(array.length){
+           const random = Math.floor(Math.random() * array.length);
+           const el = array.splice(random, 1)[0];
+           console.log(el);
+        }
+    };
+}
