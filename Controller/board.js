cf480e5d358427fae655a603a3783f02c76826f8
Niv <niv@shomron.co>
2022-07-03 17:29:50 +0300

    created function to return the position of the empty cell

--- Controller/board.js
+++ Controller/board.js
@@ -13,11 +13,21 @@ class Board {
         board[x][y] = value;
     }
 
+    getEmptyCell() {
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (board[i][j] == undefined) {
+                    return (x, y);
+                }
+            }
+        }
+    }
+
     getSwitchesCount(shuffledGameNumbers) {
         var switchesCount = 0;
         shuffledGameNumbers.forEach(number => {
             for (let i = 0; i < this.size; i++) {
-                for (let j = i; i < this.size; j++) {
+                for (let j = i; j < this.size; j++) {
                     if (board[i][j] < number) {
                         switchesCount++;
                     }
