e4647dc3d4c8f70e38a429b58dd26213bad005aa
Niv <niv@shomron.co>
2022-07-03 17:18:20 +0300

    created constructor to initialize numbers array

---
+++ Controller/board-creator.js
@@ -0,0 +1,28 @@
+class BoardCreator {
+    constructor(board, arrayShuffler, boardValidator) {
+        this.board = board;
+        this.arrayShuffler = arrayShuffler;
+        this.boardValidator = boardValidator;
+
+        var gameNumbers = [];
+
+        for(var i = 0; i < board.size; i++) {
+            matrix[i] = new Array(board.size);
+        }
+
+        for(var i = 0; i < board.size * board.size; i++) {
+            gameNumbers.push(i + 1);
+        }
+
+        this.removeRandom(gameNumbers);
+        this.shuffledGameNumbers = arrayShuffler.shuffle(gameNumbers);
+    }
+
+    removeRandom = (array) => {
+        while(array.length){
+           const random = Math.floor(Math.random() * array.length);
+           const el = array.splice(random, 1)[0];
+           console.log(el);
+        }
+     };
+}
