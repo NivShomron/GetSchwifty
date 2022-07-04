cc1dbc06e8de79750b2eeb0682ee5b1e542aabb1
Niv <niv@shomron.co>
2022-07-04 14:42:37 +0300

    changed coordinates calculation to round division down

--- View/board-updater.js
+++ View/board-updater.js
@@ -8,7 +8,7 @@ class BoardUpdater {
         var allButtons = document.getElementsByTagName("*");
         for (let i = 0; i < allButtons.length; i++) {
             var button = allButtons.item(i);
-            button.innerHTML = board.cellAt(i / 3, i % 3);
+            button.innerHTML = board.cellAt(Math.floor(i / 3), i % 3);
         }
 
         if (this.switchesCounter.count() == 0) {
