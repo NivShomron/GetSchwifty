bd642dbd0d3791ce34199cbcd1e189b751484600
Niv <niv@shomron.co>
2022-07-04 10:52:10 +0300

    fixed insertion to board

--- Controller/board-creator.js
+++ Controller/board-creator.js
@@ -8,8 +8,10 @@ class BoardCreator {
     create() {
         do {
             var cellsValues = this.cellsValuesCreator.create(this.board);
+            var insertPoisiton = 0;
             cellsValues.forEach(number => {
-                this.board.addCell(number)
+                this.board.addCell(insertPoisiton/3, insertPoisiton%3, number)
+                insertPoisiton++;
             });
         } while (this.boardValidator.validate(this.board.getSwitchesCount()));
     }
