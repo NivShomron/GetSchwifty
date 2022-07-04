fa2f4c091d93190be8f0c710a87e683d6c4d2be4
Niv <niv@shomron.co>
2022-07-04 10:40:00 +0300

    added board update function

--- View/board-display.js
+++ View/board-display.js
@@ -1,20 +1,57 @@
-import ActionValidator from "./Controller/action-validator"
-function display(actionValidator) {
+function display() {
     var size = 3;
-    for(let i = 1; i <= size; i++) { 
+
+    for(let i = 0; i < size; i++) { 
         var toAdd = document.createDocumentFragment();
         var div = document.createElement("div")
         toAdd.appendChild(div);
-        for(let j = 1; j <= size; j++){
-            var btn = document.createElement("button");
+
+        for(let j = 0; j < size; j++){
+            let btn = document.createElement("button");
+
             btn.innerHTML = 0; 
+            // btn.innerHTML = board[i][j];
             btn.addEventListener("click", () => {
                 alert(i);
                 alert(j);
-                actionValidator.validate([i, j]);
+                // actionExecuter.execute([i, j]);
+                // boardUpdater.update(board);
             });
             toAdd.appendChild(btn);
         }
         document.body.appendChild(toAdd);
     }
 }
+
+/*
+class BoardDisplay {
+    constructor(actionExecuter) {
+        this.actionExecuter = actionExecuter;
+    }
+
+    display(board) {
+        var size = 3;
+
+        for(let i = 0; i < size; i++) { 
+            var toAdd = document.createDocumentFragment();
+            var div = document.createElement("div")
+            toAdd.appendChild(div);
+    
+            for(let j = 0; j < size; j++){
+                let btn = document.createElement("button");
+    
+                btn.innerHTML = 0; 
+                //btn.innerHTML = board[i][j];
+                btn.addEventListener("click", () => {
+                    alert(i);
+                    alert(j);
+                    // this.actionExecuter.execute(btn, [i, j]);
+                    // boardUpdater.update(board);
+                });
+                toAdd.appendChild(btn);
+            }
+            document.body.appendChild(toAdd);
+        }
+    }
+}
+*/
