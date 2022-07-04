98e2233ecb886a66fb691f1199bf62ddd7063ccb
Niv <niv@shomron.co>
2022-07-04 11:15:26 +0300

    merged feature/size-input

--- View/user-input.js
+++ View/user-input.js
@@ -7,7 +7,6 @@ class UserInput {
         do {
             var input = prompt("How big do you want the board to be?", 3)
         } while (this.inputValidator.validate(input));          
-        
         return input;
     }
 }
