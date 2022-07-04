d073c2e420a8099d577a88cb7e101b3b435dedb1
Niv <niv@shomron.co>
2022-07-04 11:40:02 +0300

    created simple score class

---
+++ Model/score.js
@@ -0,0 +1,13 @@
+class Score {
+    constructor(name, gameLength, boardSize, startTime) {
+        this.rank = -1;
+        this.name = name;
+        this.gameLength = gameLength;
+        this.boardSize = boardSize;
+        this.startTime = startTime;
+    }
+
+    changeRank(rank) {
+        this.rank = rank; 
+    }
+}
