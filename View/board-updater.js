24864bacf3476b85adf4a060eba4aa963f472813
Niv <niv@shomron.co>
2022-07-04 14:39:38 +0300

    added 'var' to state that creating a new variable

--- View/board-updater.js
+++ View/board-updater.js
@@ -7,7 +7,7 @@ class BoardUpdater {
     update(board) {
         var allButtons = document.getElementsByTagName("*");
         for (let i = 0; i < allButtons.length; i++) {
-            button = allButtons.item(i);
+            var button = allButtons.item(i);
             button.innerHTML = board.cellAt(i / 3, i % 3);
         }
 
