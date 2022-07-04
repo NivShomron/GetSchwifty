afb722103df93e49291a7be38482c88c7e2df998
Niv <niv@shomron.co>
2022-07-04 14:22:25 +0300

    changed variables names to fit current variables used

--- Controller/cells_values_creator.js
+++ Controller/cells_values_creator.js
@@ -8,14 +8,14 @@ class CellsValuesCreator {
         var gameNumbers = [];
 
         for(var i = 0; i < this.board.size; i++) {
-            matrix[i] = new Array(this.board.size);
+            this.board[i] = new Array(this.board.size);
         }
 
         for(var i = 0; i < this.board.size * this.board.size; i++) {
             gameNumbers.push(i + 1);
         }
 
-        gameNumbers.splice(Math.floor(Math.random()*array.length), 1);
+        gameNumbers.splice(Math.floor(Math.random()*gameNumbers.length), 1);
         return this.arrayShuffler.shuffle(gameNumbers);
     }
 }
