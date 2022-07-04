562a386a58a4e94d5c0cf5a0f9667f1d62a04e61
Niv <niv@shomron.co>
2022-07-04 11:39:47 +0300

    created leaderboards manipulation functions

---
+++ Model/leaderboard.js
@@ -0,0 +1,27 @@
+class Leaderboard {
+    constructor(leaderboard, leaderboardSize, sortStrategy) {
+        this.leaderboard = leaderboard;
+        this.leaderboardSize = leaderboardSize;
+        this.sortStrategy = sortStrategy;
+    }
+
+    addScore(newScore) {
+        if (this.leaderboard.length == this.leaderboardSize) {
+            let lastScore = this.leaderboard.pop();
+            lastScore.changeRank(-1);
+        }
+        this.leaderboard.push(newScore);
+        this.reorderLeaderboard();
+    }
+
+    reorderLeaderboard() {
+        this.sortStrategy.sort(this.leaderboard);
+        this.updateRanks();
+    }
+
+    updateRanks() {
+        for (let i = 0; i < this.leaderboard.length; i++) {
+            this.leaderboard[i].changeRank(i + 1);
+        }
+    }
+}
