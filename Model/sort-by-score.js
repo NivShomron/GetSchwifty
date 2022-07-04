eb0fc3db4cff28fb10bb9524ef7f6d9ab835fe22
Niv <niv@shomron.co>
2022-07-04 11:40:43 +0300

    created learderboards sorting function by score

---
+++ Model/sort-by-score.js
@@ -0,0 +1,11 @@
+class SortByScore {
+    sort(leaderboard) {
+        return leaderboard.sort(
+            function(score1, score2) {
+            var calculatedScore1 = score1[gameLength] / score1[boardSize];
+            var calculatedScore2 = score2[gameLength] / score2[boardSize];
+            return ((calculatedScore1 < calculatedScore2) ? -1 : 
+                ((calculatedScore1 > calculatedScore2) ? 1 : 0));
+        });
+    }
+}
