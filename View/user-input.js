97bf6e92f902f9286e2b5103fa20bff252c2a0fb
Niv <niv@shomron.co>
2022-07-04 16:13:33 +0300

    created function to receive name from user

--- View/user-input.js
+++ View/user-input.js
@@ -2,8 +2,12 @@ class UserInput {
     constructor(inputValidator) {
         this.inputValidator = inputValidator;
     }
+    
+    receiveName() {
+        var name = prompt("What is your name?")
+    }
 
-    receiveInput() {
+    receiveBoardSize() {
         do {
             var input = prompt("How big do you want the board to be?", 3)
         } while (!this.inputValidator.validate(input));          
