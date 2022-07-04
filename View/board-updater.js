ca2bf385cb16cf3539fd6d23a81e5e3965c5d43c
Niv <niv@shomron.co>
2022-07-04 12:16:44 +0300

    created a check for every update if game has ended

--- View/board-updater.js
+++ View/board-updater.js
@@ -1,10 +1,19 @@
 class BoardUpdater {
+    constructor(switchesCounter, endGame) {
+        this.switchesCounter = switchesCounter;
+        this.endGame = endGame;
+    }
+
     update(board) {
         var allButtons = document.getElementsByTagName("*");
 
         for (let i = 0; i < allButtons.length; i++) {
             button = allButtons.item(i);
             button.innerHTML = board.cellAt(i / 3, i % 3);
+        }
+
+        if (this.switchesCounter.count() == 0) {
+            this.endGame.end();
         }
     }
 }
