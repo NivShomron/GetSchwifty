b47b51affaee3b21c4d55308ed9a57cba4bdd97e
Niv <niv@shomron.co>
2022-07-04 14:19:05 +0300

    removed code from function

--- bootstrapper.js
+++ bootstrapper.js
@@ -1,47 +1,45 @@
-import ActionExecuter from "./Controller/action-executer"
-import ActionValidator from "./Controller/action-validator"
-import ArrayShuffler from "./Controller/array-shuffler"
-import BoardCreator from "./Controller/board-creator"
-import BoardValidator from "./Controller/board-validator"
-import Board from "./Controller/board"
-import CellsValuesCreator from "./Controller/cells_values_creator"
-import GameDisplay from "./View/game-display"
-import InputValidator from "./Controller/input-validator"
-import UserInput from "./View/user-input"
-import Leaderboard from "./Model/leaderboard"
-import SortByScore from "./Model/sort-by-score"
-import EndGame from "./Controller/end-game"
-import SwitchesCounter from "./Controller/switches-counter"
-import BoardUpdater from "./View/board-updater"
+import { ActionExecuter } from "./Controller/action-executer.js";
+import { ActionValidator } from "./Controller/action-validator.js";
+import { ArrayShuffler } from "./Controller/array-shuffler.js";
+import { BoardCreator } from "./Controller/board-creator.js";
+import { BoardValidator } from "./Controller/board-validator.js";
+import { Board } from "./Controller/board.js";
+import { CellsValuesCreator } from "./Controller/cells_values_creator.js";
+import { GameDisplay } from "./View/game-display.js";
+import { InputValidator } from "./Controller/input-validator.js";
+import { UserInput } from "./View/user-input.js";
+import { Leaderboard } from "./Model/leaderboard.js";
+import { SortByScore } from "./Model/sort-by-score.js";
+import { EndGame } from "./Controller/end-game.js";
+import { SwitchesCounter } from "./Controller/switches-counter.js";
+import { BoardUpdater } from "./View/board-updater.js";
 
-function init() {
-    var leaderboard = []
-    var leaderboardSize = 5;
-    var sortByScore = new SortByScore();
+var starting_leaderboard = []
+var leaderboardSize = 5;
+var sortByScore = new SortByScore();
 
-    var leaderboard = new Leaderboard(leaderboard, leaderboardSize, sortByScore);
+var leaderboard = new Leaderboard(starting_leaderboard, leaderboardSize, sortByScore);
 
-    var inputValidator = new InputValidator();
-    var userInput = new UserInput(inputValidator);
-    var size = userInput.receiveInput();
+var inputValidator = new InputValidator();
+var userInput = new UserInput(inputValidator);
+var size = userInput.receiveInput();
 
-    var board = new Board(size);
+var board = new Board(size);
 
-    var boardValidator = new BoardValidator();
-    var arrayShuffler = new ArrayShuffler();
+var boardValidator = new BoardValidator();
+var arrayShuffler = new ArrayShuffler();
 
-    var switchesCounter = new SwitchesCounter(board);
-    var endGame = new EndGame();
+var switchesCounter = new SwitchesCounter(board);
+var endGame = new EndGame();
 
-    var cellsValuesCreator = new CellsValuesCreator(board, arrayShuffler)
-    var boardCreator = new BoardCreator(board, boardValidator, cellsValuesCreator, switchesCounter);
+var cellsValuesCreator = new CellsValuesCreator(board, arrayShuffler)
+var boardCreator = new BoardCreator(board, boardValidator, cellsValuesCreator, switchesCounter);
 
-    var actionValidator = new ActionValidator(board);
-    var actionExecuter = new ActionExecuter(board, actionValidator);
+var actionValidator = new ActionValidator(board);
+var actionExecuter = new ActionExecuter(board, actionValidator);
 
-    var boardUpdater = new BoardUpdater(switchesCounter, endGame);
+var boardUpdater = new BoardUpdater(switchesCounter, endGame);
 
-    boardCreator.create();
-    var gameDisplay = new GameDisplay(actionExecuter, boardUpdater);
-    gameDisplay.display(board);
-}
+boardCreator.create();
+var gameDisplay = new GameDisplay(actionExecuter, boardUpdater);
+gameDisplay.display(board);
