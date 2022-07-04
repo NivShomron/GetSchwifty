4b03310b248515a2d2634746a9d564b305b0ae5a
Niv <niv@shomron.co>
2022-07-04 19:42:47 +0300

    changed if statement to check if number is also an invalid integer

--- Controller/input-validators/integer-input-validator.js
+++ Controller/input-validators/integer-input-validator.js
@@ -1,6 +1,6 @@
 class IntegerInputValidator {
     validate(input) {
-        if (input == null) {
+        if (input == null || input <= 1) {
             return false;
         }
         return parseInt(input);
