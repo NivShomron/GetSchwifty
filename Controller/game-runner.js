94c7036057434caa865dd03a8d9b539944b802e4
Niv <niv@shomron.co>
2022-07-04 17:11:03 +0300

    added score to leaderboard after game ended

--- Controller/game-runner.js
+++ Controller/game-runner.js
@@ -1,13 +1,30 @@
 class GameRunner {
-    constructor(board, boardCreator, gameDisplay) {
+    constructor(board, boardCreator, gameDisplay, leaderboard, stopwatch, 
+        scoreCreator, userInput) {
         this.board = board;
         this.boardCreator = boardCreator;
         this.gameDisplay = gameDisplay;
+        this.leaderboard = leaderboard;
+        this.stopwatch = stopwatch;
+        this.scoreCreator = scoreCreator;
+        this.userInput = userInput;
     }
     
     run() {
+        var startTime = Date.now();
+        this.stopwatch.startStopwatch();
+
         this.boardCreator.create(this.board);
         this.gameDisplay.display(this.board);
+        
+        this.stopwatch.stop()
+
+        var name = this.userInput.receiveName();
+        
+        var score = this.scoreCreator.create(name, this.stopwatch.elapsedTime, 
+            this.board.size, startTime);
+            
+        this.leaderboard.addScore(score);
     }
 }
 
