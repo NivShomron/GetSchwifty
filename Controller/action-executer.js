85acf16253b62acf451a5802faa7a8a5783a8874
Niv <niv@shomron.co>
2022-07-04 10:56:07 +0300

    changed access to board to be from cellAt function

--- Controller/action-executer.js
+++ Controller/action-executer.js
@@ -6,11 +6,11 @@ class ActionExecuter {
 
     execute(cellDecision) {
         if (this.actionValidator.validate(cellDecision)){
-            var cellValue = this.board[cellDecision[0]][cellDecision[1]];
+            var cellValue = this.board.cellAt(cellDecision[0], cellDecision[1]);
             var emptyCell = this.board.getEmptyCell();
     
-            this.board[emptyCell[0]][emptyCell[1]] = cellValue;
-            this.board[cellDecision[0]][cellDecision[1]] = undefined;
+            this.board.cellAt(emptyCell[0], emptyCell[1]) = cellValue;
+            this.board.cellAt(cellDecision[0], cellDecision[1]) = undefined;
         }
     }
 }
