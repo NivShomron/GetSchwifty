8d2f1e7cb087c38fadbbca72e77b58d69c89b8c6
Niv <niv@shomron.co>
2022-07-04 15:49:01 +0300

    added code to display h1 text when function is called

--- Controller/end-game.js
+++ Controller/end-game.js
@@ -1,6 +1,11 @@
 class EndGame {
     end(buttons) {
         Array.prototype.forEach.call(buttons, button => { button.disabled = true; });
+
+        var h1Tag = document.createElement("H1");
+        var h1Text = document.createTextNode("YOU WIN!");
+        h1Tag.appendChild(h1Text);
+        document.body.appendChild(h1Tag);
     }
 }
 
