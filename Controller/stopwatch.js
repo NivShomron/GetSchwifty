3ac830477333ea2ca0186104067417b4b2daf16f
Niv <niv@shomron.co>
2022-07-04 17:36:51 +0300

    refactoring

--- Controller/stopwatch.js
+++ Controller/stopwatch.js
@@ -1,27 +1,19 @@
 class Stopwatch {
     constructor() {
-        this.stopwatch = { elapsedTime: 0 }
+        this.elapsedTime = 0;
     }
 
     start() {
-        this.stopwatch.startTime = Date.now();
-        this.stopwatch.intervalId = setInterval(() => {
-            const elapsedTime = Date.now() - this.stopwatch.startTime + this.stopwatch.elapsedTime
-            const milliseconds = parseInt((elapsedTime%1000)/10)
-            const seconds = parseInt((elapsedTime/1000)%60)
-            const minutes = parseInt((elapsedTime/(1000*60))%60)
-            const hour = parseInt((elapsedTime/(1000*60*60))%24);
-        }, 100);
+        this.startTime = Date.now();
     }
 
     stop() {
-        this.stopwatch.elapsedTime += Date.now() - this.stopwatch.startTime
-        clearInterval(this.stopwatch.intervalId)
+        this.elapsedTime += Date.now() - this.startTime
     }
 
     reset() {
-        this.stopwatch.elapsedTime = 0
-        this.stopwatch.startTime = Date.now()
+        this.elapsedTime = 0
+        this.startTime = Date.now()
     }
 }
 
