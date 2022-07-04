f879a7accad587014c4a0a3f74426ace41e1e368
Niv <niv@shomron.co>
2022-07-04 15:26:42 +0300

    changed code to only count switches if they are not with the empty cell

--- Controller/switches-counter.js
+++ Controller/switches-counter.js
@@ -8,7 +8,8 @@ class SwitchesCounter {
         cellsValues.forEach(number => {
             for (let i = 0; i < this.board.size; i++) {
                 for (let j = i; j < this.board.size; j++) {
-                    if (this.board.getCellAt(i, j) < number) {
+                    if (this.board.getEmptyCell() != [i, j] && 
+                            this.board.getCellAt(i, j) < number) {
                         switchesCount++;
                     }
                 }
