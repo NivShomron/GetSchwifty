0613b9724fc86efdccc68817ab69bb6692ae6192
Niv <niv@shomron.co>
2022-07-04 15:37:13 +0300

    fixed function to check if empty cell coordinates are equal to current coordinates

--- Controller/switches-counter.js
+++ Controller/switches-counter.js
@@ -8,7 +8,7 @@ class SwitchesCounter {
         cellsValues.forEach(number => {
             for (let i = 0; i < this.board.size; i++) {
                 for (let j = i; j < this.board.size; j++) {
-                    if (this.board.getEmptyCell() != [i, j] && 
+                    if (this.board.getEmptyCell()[0] != i &&  this.board.getEmptyCell()[1] != j &&
                             this.board.getCellAt(i, j) < number) {
                         switchesCount++;
                     }
