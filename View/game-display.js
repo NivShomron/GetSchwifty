73b63a564036d905eed64070095cc99865706060
Niv <niv@shomron.co>
2022-07-04 14:53:33 +0300

    removed unused code

--- View/game-display.js
+++ View/game-display.js
@@ -1,30 +1,3 @@
-/*
-function display() {
-    var size = 3;
-
-    for(let i = 0; i < size; i++) { 
-        var toAdd = document.createDocumentFragment();
-        var div = document.createElement("div")
-        toAdd.appendChild(div);
-
-        for(let j = 0; j < size; j++){
-            let btn = document.createElement("button");
-
-            btn.innerHTML = 0; 
-            // btn.innerHTML = board[i][j];
-            btn.addEventListener("click", () => {
-                alert(i);
-                alert(j);
-                // actionExecuter.execute([i, j]);
-                // boardUpdater.update(board);
-            });
-            toAdd.appendChild(btn);
-        }
-        document.body.appendChild(toAdd);
-    }
-}
-*/
-
 class GameDisplay {
     constructor(actionExecuter, boardUpdater) {
         this.actionExecuter = actionExecuter;
