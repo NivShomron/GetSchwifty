a1d5fe7240f10cd9d065efe3cec42020e2c83d5b
Niv <niv@shomron.co>
2022-07-04 20:08:40 +0300

    rafactoring

--- Controller/board-creation/board-validator.js
+++ Controller/board-creation/board-validator.js
@@ -1,10 +1,10 @@
 class BoardValidator {  
-    validate(board, switchesCount, emptyCellRow, boardChecker) {
+    validate(board, switchesCount, boardChecker) {
         if (boardChecker.ended(board.getFlattenedBoard())) {
             return false;
         }
         if (board.size % 2 == 0) {
-            if (switchesCount + emptyCellRow % 2 == 0) {
+            if ((switchesCount + board.getEmptyCell()[1]) % 2 == 0) {
                 return true;
             }
             return false;
