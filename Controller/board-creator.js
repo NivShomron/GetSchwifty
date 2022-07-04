c73764a07a78f829ee8d9a8c4ce1f9f16b5dd24c
Niv <niv@shomron.co>
2022-07-04 17:24:17 +0300

    refactoring

--- Controller/board-creator.js
+++ Controller/board-creator.js
@@ -10,10 +10,12 @@ class BoardCreator {
             var cellsValues = this.cellsValuesCreator.create();
             var insertPoisiton = 0;
             cellsValues.forEach(number => {
-                board.addCell(Math.floor(insertPoisiton / board.size), insertPoisiton % board.size, number)
+                board.addCell(Math.floor(insertPoisiton / board.size), insertPoisiton % board.size,
+                 number)
                 insertPoisiton++;
             });
-        } while (this.boardValidator.validate(board.size, this.switchesCounter.count(cellsValues), board.getEmptyCell()[1]));
+        } while (this.boardValidator.validate(board.size, this.switchesCounter.count(cellsValues),
+             board.getEmptyCell()[1]));
     }
 }
 
